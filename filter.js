70fe913f60c9c16d33f1c8f7d9e5e8476839ef76
VarnikaB <1rn18cs115.varnika@gmail.com>
2022-07-26 12:51:54 +0530

    filter without filter

---
+++ filter.js
@@ -0,0 +1,10 @@
+function filter(array, callback_function) {
+  const result = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (callback_function(array[i])) {
+      result.push(array[i]);
+    }
+  }
+  return result;
+}
+console.log(filter([1, 2, 3, 4, 5], item => item >= 4)); // [4, 5]
